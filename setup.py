b872ca137e7b55f11e365e33bb395146103cf0a1
dparalen <vetrisko@gmail.com>
2015-02-21 09:57:09 +0100

    fix: preserve order of request arrival

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='requestbin',
-    version=0.1,
+    version=0.2,
     description='a silly wrapper around requestb.in api',
     author='dparalen',
     license='GPLv3+',
